2fefe776cfd27f87b5551e5309e4cb2b989f17e7
kololo21 <harurunking38f@gmail.com>
2026-06-29 14:33:17 +0900

    feat: add BookPanel with JTable

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -1,7 +1,12 @@
 package ui;
 
 import ui.panel.*;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import service.BookService;
@@ -18,6 +23,7 @@ public class MainFrame extends JFrame{
         tabs.addTab("Members",new MemberPanel(ms,ls));
         tabs.addTab("Loan History",new LoanHistoryPanel(ls));
         add(tabs);
+       
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
--- src/ui/panel/Bookpanel.java
+++ src/ui/panel/Bookpanel.java
@@ -23,11 +23,34 @@ public class BookPanel extends JPanel {
 
         this.model = new DefaultTableModel(columns,0);
         table = new JTable(model);
-
         setLayout(new BorderLayout());
         JScrollPane scroll = new JScrollPane(table);
         add(scroll, BorderLayout.CENTER);  // move table to CENTER
         refreshTable();
+        
+         //Button Event
+        JButton addButton = new JButton("Add Book");
+        addButton.addActionListener(e->{
+            //event process when click on
+            System.out.println("Add clicked");
+        });
+        JButton editButton = new JButton("Edit Book");
+        addButton.addActionListener(e->{
+            //event process when click on
+            System.out.println("Edit clicked");
+
+        });
+        JButton deleteButton = new JButton("Delete Book");
+        addButton.addActionListener(e->{
+            //event process when click on
+            System.out.println("Delete clicked");
+
+        });
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(addButton);
+        buttonPanel.add(editButton);
+        buttonPanel.add(deleteButton);
+        add(buttonPanel,BorderLayout.SOUTH);
     }
     public void refreshTable(){
         model.setRowCount(0); //all lines clear
